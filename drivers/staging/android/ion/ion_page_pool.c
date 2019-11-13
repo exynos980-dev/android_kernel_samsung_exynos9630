70601871481a173cf1a5ff5c5289a24d744efa5b
Suren Baghdasaryan <surenb@google.com>
2019-11-13 10:45:00 -0800

    ANDROID: staging: android: ion: Expose total heap and pool sizes via sysfs
    Add sysfs attributes to track ion total heap and pool memory allocations.
    The following sysfs attributes are added:
    
    /sys/kernel/ion/total_heaps_kb
    /sys/kernel/ion/total_pools_kb
    
    Bug: 138148041
    Test: adb shell cat /sys/kernel/ion/*
    Change-Id: If92770dc3389af865c619525f04d3ba0e013b244
    Signed-off-by: Suren Baghdasaryan <surenb@google.com>

--- drivers/staging/android/ion/ion_page_pool.c
+++ drivers/staging/android/ion/ion_page_pool.c
@@ -14,6 +14,13 @@
 
 #include "ion.h"
 
+/*
+ * We avoid atomic_long_t to minimize cache flushes at the cost of possible
+ * race which would result in a small accounting inaccuracy that we can
+ * tolerate.
+ */
+static long nr_total_pages;
+
 static void *ion_page_pool_alloc_pages(struct ion_page_pool *pool, bool nozero)
 {
 	gfp_t gfpmask = pool->gfp_mask;
@@ -51,6 +58,7 @@ static void ion_page_pool_add(struct ion_page_pool *pool, struct page *page)
 		pool->low_count++;
 	}
 
+	nr_total_pages += 1 << pool->order;
 	mod_node_page_state(page_pgdat(page), NR_INDIRECTLY_RECLAIMABLE_BYTES,
 			    (1 << (PAGE_SHIFT + pool->order)));
 	mutex_unlock(&pool->mutex);
@@ -71,6 +79,7 @@ static struct page *ion_page_pool_remove(struct ion_page_pool *pool, bool high)
 	}
 
 	list_del(&page->lru);
+	nr_total_pages -= 1 << pool->order;
 	mod_node_page_state(page_pgdat(page), NR_INDIRECTLY_RECLAIMABLE_BYTES,
 			    -(1 << (PAGE_SHIFT + pool->order)));
 	return page;
@@ -136,6 +145,14 @@ static int ion_page_pool_total(struct ion_page_pool *pool, bool high)
 		count += pool->high_count;
 
 	return count << pool->order;
+}
+
+long ion_page_pool_nr_pages(void)
+{
+	/* Correct possible overflow caused by racing writes */
+	if (nr_total_pages < 0)
+		nr_total_pages = 0;
+	return nr_total_pages;
 }
 
 int ion_page_pool_shrink(struct ion_page_pool *pool, gfp_t gfp_mask,
